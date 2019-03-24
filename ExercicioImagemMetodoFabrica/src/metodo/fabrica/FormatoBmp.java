e95ab9773d329ef3d6cbbcb489b66da0892cf11a
Jardielma Queiroz de Lima <jardielmaqueiroz@hotmail.com>
2019-03-23 23:47:40 -0300

    Exercicio Imagem implementando padrão de projeto (Método Fabrica)

---
+++ ExercicioImagemMetodoFabrica/src/metodo/fabrica/FormatoBmp.java
@@ -0,0 +1,15 @@
+package metodo.fabrica;
+
+/**
+ *
+ * @author jardielma
+ */
+public class FormatoBmp implements Formato{
+    
+    @Override
+    public Imagem gera() {
+        System.out.println("gera bmp");
+        return null;
+    }
+    
+}
