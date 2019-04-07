455eaa37b8ae9a2389993b2cd96fa1eac522c4da
Jardielma Queiroz de Lima <jardielmaqueiroz@hotmail.com>
2019-04-06 21:34:58 -0300

    Exercicio padrão de projeto
    Exercicio padrão de projeto

---
+++ ExercicioFabricaAbstrataTemaInterface/src/fabrica/abstrata/botoes/FabricaBotoesPadrao.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fabrica.abstrata.botoes;
+
+import javax.swing.JButton;
+
+/**
+ *
+ * @author jardielma
+ */
+public class FabricaBotoesPadrao extends FabricaAbstrataBotoes {
+    
+    @Override
+    public JButton criaBotaoOK(){
+        JButton ok = new JButton();
+        ok.setText("OK");
+        return ok;
+    }
+    
+    @Override
+    public JButton criaBotaoCancel(){
+        JButton cancel = new JButton();
+        cancel.setText("Cancel");
+        return cancel;
+    }        
+}
